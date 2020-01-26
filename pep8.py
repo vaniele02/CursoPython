5417a3c3d02598c5856a3061c1e080b477ec504f
Vaniele Pizzol <pizzol.vaniele@gmail.com>
2020-01-26 15:21:42 -0300

    Pep8

---
+++ pep8.py
@@ -0,0 +1,36 @@
+"""
+ Pep8 - Python Enchancement Proposal
+São propostas de melhorias para a linguagem Python
+The Zen of Python
+import this
+A ideia da Pep8 é que possamos escrever códigos Python de forma Pythomica
+1- Utilize Camel Case para nomes de classes;
+
+2- Utilize nomes em minúsculo, separado por underline para funções ou variáveis;
+
+3- Utilize 4 espaços para identação! Não é recomendado usar o Tab!
+
+4- linhas em branco, separar funções e  classe deixar duas lihas em branco, métodos dentro de uma classe
+devem ser separados apenas por 1 linha em branco
+
+5- imports devem ser feitas em linhas separadas
+#não há problema em utilizar:
+#from types import StringType, ListType
+#Caso tenha muitos imports de um mesmo pacaoe, recomenda-se fazer:
+# from types import{ }
+#imports decem ser colocados no topo do arquivo depois de
+qualquer comenarios ou docstrings,antes de qualquer variavel global
+
+6- Espaços em expressões e instruções, não deixe espaços em branco
+
+7- termine sempre uma instruçao com uma nova linha
+"""
+
+
+
+
+
+
+
+
+
