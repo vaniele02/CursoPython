b3d2219572aac955215012b475dc45eb62ee2d19
Vaniele Pizzol <pizzol.vaniele@gmail.com>
2020-01-26 22:44:59 -0300

    Dir_Help

---
+++ Dir_Help.py
@@ -0,0 +1,15 @@
+"""
+Utilitários Python para que auxiliar na programação
+Dir-> Apreseta todos os atributos/probriedades e funções/métodos disponíveis
+ para determinado tipo de dado ou variável
+
+dir(tipo de dado/variável)
+Ex.:dir("Vaniele")
+
+Help->Apreseta a documentação/como utilizar os atributos/propriedades e
+funções/métodos disponíveis para determinado tipo de dado ou variável.
+
+help(tipo de dado/variável.propriedade)
+
+
+"""
