5bae1e3966994b9be74b2c4207ca6b940aa940d4
Vaniele Pizzol <pizzol.vaniele@gmail.com>
2020-02-03 22:10:00 -0300

    Second version

--- tipo_booleano.py
+++ tipo_booleano.py
@@ -26,3 +26,28 @@ Fazendo a negaçao, se o valor  boolenao for verdadeiro  o resultado
 sera falso
 Se o for falso o resultado será verdadeiro. Ou seja, sempre o contrário 
 """
+print(not ativo)
+logado = False
+
+#ou (or)
+"""
+É uma operação binária, ou seja, depende de dois valores.
+Ou um ou oureo deve ser verdadeiro.
+True or True -> True
+True or False-> True 
+False or True-> True 
+False or False -> False
+
+"""
+print(ativo or logado)
+
+#E (and):
+"""
+Também é uma operação binária. ou seja, depende de dois valores.
+Ambos os valores devem ser verdadeiro\
+
+True and True -> True
+True and False -> False
+False and True -> False
+False and False -> False
+"""
