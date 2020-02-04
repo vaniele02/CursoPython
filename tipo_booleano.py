f7fe26237cf200f1fcabe2c49061b328b1d64d68
Vaniele Pizzol <pizzol.vaniele@gmail.com>
2020-02-03 21:56:03 -0300

    First Version

---
+++ tipo_booleano.py
@@ -0,0 +1,28 @@
+"""
+ Tipo Booleano
+
+ Algebra Booleana, criada por Gorge Boole
+
+ 2constates, Verdadeiro ou Flaso
+ True-> Verdadeiro
+ False -> Falso
+ OBS: Sempre com a inicial maiúscula
+ Errado:
+ true, false
+
+ Certo:
+ True, False
+"""
+
+ativo = False
+print(ativo)
+
+"""
+Operações básicas:
+"""
+#Negação (not):
+"""
+Fazendo a negaçao, se o valor  boolenao for verdadeiro  o resultado
+sera falso
+Se o for falso o resultado será verdadeiro. Ou seja, sempre o contrário 
+"""
